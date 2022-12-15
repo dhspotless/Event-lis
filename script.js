3484f78e3abf59a8a7d9c8997626970e2d88ed9c
dhspotless <dhspotless@gmail.com>
2022-12-15 18:27:15 +0100

    new committ

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+let colors = document.querySelector('button')
+
+
+// colors.addEventListener('click', function(){
+//     document.querySelector('h1').style.color = 'red'
+// })
+
+
+colors.addEventListener('click', doThis = () => {
+    document.querySelector('h1').style.color = 'red'
+})
